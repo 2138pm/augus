ce4a5830f42997d9e949ffe8b4754ecee58e93a0
augusyouthh <157354046+augusyouthh@users.noreply.github.com>
2024-04-17 14:56:48 +0700

    Delete BankAccountdemo1 directory

--- BankAccountdemo1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ku.cs.bankaccountdemo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ku.cs.bankaccountdemo1 to javafx.fxml;
-    exports ku.cs.bankaccountdemo1;
-}
